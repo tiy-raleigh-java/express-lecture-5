f58819425246c5b77ec9c4cf7c8b0f4b63d450cb
Doug Hughes <doug@doughughes.net>
2017-07-31 13:31:46 -0400

    factored routers from index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const session = require('express-session');
 const fs = require('fs');
 const app = express();
 
+// require our food router
+const foodRoutes = require('./routes/food');
+const numberRoutes = require('./routes/number');
+const rootRoutes = require('./routes/root');
+
 // tell express to use handlebars
 app.engine('handlebars', exphbs());
 app.set('views', './views');
@@ -38,84 +43,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // other methods to the req.
 app.use(expressValidator());
 
-// this middleware creates a default session
-app.use((req, res, next) => {
-  // if we don't already have an array of foods
-  if (!req.session.foods) {
-    // then create an empty array of foods
-    req.session.foods = [];
-  }
+// add our root routes
+app.use('/', rootRoutes);
 
-  console.log(req.session);
+// add our food-related routes
+app.use('/food', foodRoutes);
 
-  next();
-});
-
-// configure the webroot
-app.get('/', function(req, res) {
-  res.render('home', {
-    formattedDate: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-    foods: req.session.foods
-  });
-});
-
-// show a particular food
-app.get('/food/:index', function(req, res) {
-  res.render('food', {
-    foodItem: req.session.foods[req.params.index]
-  });
-});
-
-// show a random number
-app.get(
-  // this means to listen for a request to http://..../randomNumber
-  '/randomNumber/:max',
-  // this is the code that gets run when the request comes in
-  function(req, res) {
-    // generate my random number
-    let myRandomNumber = Math.round(Math.random() * req.params.max);
-
-    // render the randomNumber template using the provided information
-    res.render('randomNumber', {
-      randomNumber: myRandomNumber
-    });
-  }
-);
-
-// this endpoint will show a form I can use to add foods
-app.get('/foodForm', function(req, res) {
-  res.render('foodForm');
-});
-
-// this handles the posted data from the form and creates a new food item
-app.post('/addFood', function(req, res) {
-  // get the food item details from the posted body data
-  let foodItem = req.body;
-
-  // validate the food item's data
-  req.checkBody('name', 'Name is required').notEmpty();
-  // make sure that the serving is provided.
-  req.checkBody('serving', 'Serving is required').notEmpty();
-  // make sure that the serving is provided.
-  req.checkBody('serving', 'Serving is must uppercase').isUppercase();
-  // get all errors from our validation that we just did as an array
-  let errors = req.validationErrors();
-
-  if (errors) {
-    // there were errors, report them
-    console.log(errors);
-
-    res.render('foodForm', { errors: errors, foodItem: foodItem });
-  } else {
-    // there were no errors. save the food item
-
-    // store the food item in our array of foods
-    req.session.foods.push(foodItem);
-
-    // now that I've added the food item to the array, redirect to the homepage
-    res.redirect('/');
-  }
-});
+// add our number-related routes
+app.use('/number', numberRoutes);
 
 // make express listen on port 3000
 app.listen(3000);
