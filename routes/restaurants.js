8215ec36ad554f9030b04f58e72383fe7a57e428
Doug Hughes <doug@doughughes.net>
2017-08-02 16:29:50 -0400

    added another endpoint to search for restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,16 +2,31 @@ const express = require('express');
 const routes = express.Router();
 const db = require('../db');
 
-// show a random number
+// show all
 routes.get(
   // this means to listen for a request to http://..../randomNumber0
   '/',
   // this is the code that gets run when the request comes in
   function(req, res) {
-    let restaurants = db.get().collection('restaurants');
+    let collection = db.get().collection('restaurants');
 
-    restaurants.find().toArray((err, restaurants) => {
-      console.log(restaurants);
+    collection.find().toArray((err, restaurants) => {
+      // render the restaurants template using the restaurants
+      res.render('restaurants', {
+        restaurants
+      });
+    });
+  }
+);
+
+// show ones that match the url
+routes.get(
+  '/:pattern',
+  // this is the code that gets run when the request comes in
+  function(req, res) {
+    let collection = db.get().collection('restaurants');
+
+    collection.find({ name: new RegExp(req.params.pattern, 'i') }).toArray((err, restaurants) => {
       // render the restaurants template using the restaurants
       res.render('restaurants', {
         restaurants
